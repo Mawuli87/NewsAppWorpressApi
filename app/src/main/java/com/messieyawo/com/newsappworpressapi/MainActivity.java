b718622474b956620deec794ecbc8f1228a47035
Mawuli87 <yawomessie@gmail.com>
2023-01-08 09:49:16 +0000

    Slider with local images

---
+++ app/src/main/java/com/messieyawo/com/newsappworpressapi/MainActivity.java
@@ -0,0 +1,58 @@
+package com.messieyawo.com.newsappworpressapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bumptech.glide.request.RequestOptions;
+import com.glide.slider.library.SliderLayout;
+import com.glide.slider.library.slidertypes.BaseSliderView;
+import com.glide.slider.library.slidertypes.DefaultSliderView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    SliderLayout sliderLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        List<Integer> images = new ArrayList<>();
+        images.add(R.drawable.banner_1);
+        images.add(R.drawable.banner_2);
+        images.add(R.drawable.banner_3);
+        images.add(R.drawable.banner_4);
+
+        for (int i = 0; i< images.size(); i++){
+            DefaultSliderView defaultSliderView = new DefaultSliderView(this);
+            defaultSliderView.setRequestOption(new RequestOptions().centerCrop());
+            defaultSliderView.image(images.get(i));
+            defaultSliderView.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+                @Override
+                public void onSliderClick(BaseSliderView slider) {
+
+                }
+            });
+            sliderLayout.addSlider(defaultSliderView);
+        }
+
+        sliderLayout.startAutoCycle();
+        sliderLayout.setPresetTransformer(SliderLayout.Transformer.Accordion);//stack or accordion
+        sliderLayout.setDuration(4000);
+        sliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Right_Bottom);
+
+    }
+
+    private void initViews() {
+        sliderLayout = findViewById(R.id.slider);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sliderLayout.stopAutoCycle();
+    }
+}
